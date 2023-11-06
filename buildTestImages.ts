0584ca2ffa85ba0463e0059b57a8799836013193
Turadg Aleahmad <turadg@agoric.com>
2023-11-06 15:07:22 -0800

    ci: include scope in GHA cache

--- buildTestImages.ts
+++ buildTestImages.ts
@@ -8,13 +8,12 @@ import { refreshDockerfile } from './makeDockerfile';
 refreshDockerfile();
 
 const options = {
-  buildOpts: { type: 'string' },
   match: { short: 'm', type: 'string' },
   dry: { type: 'boolean' },
 } as const;
 const { values } = parseArgs({ options });
 
-const { buildOpts, match, dry } = values;
+const { match, dry } = values;
 
 const allProposals = readProposals();
 
@@ -22,14 +21,24 @@ const proposals = match
   ? allProposals.filter(p => p.proposalName.includes(match))
   : allProposals;
 
+function buildOptsFor(name: string) {
+  // use GitHub Actions Cache if enabled https://docs.docker.com/build/cache/backends/gha/
+  if (process.env.ACTIONS_RUNTIME_TOKEN) {
+    const refName = process.env.GITHUB_REF_NAME;
+    return `--cache-from type=gha,scope=${refName}-${name} --cache-to type=gha,mode=max,scope=${refName}-${name}`;
+  }
+  return '';
+}
+
 for (const proposal of proposals) {
   if (!dry) {
     console.log(`\nBuilding test image for proposal ${proposal.proposalName}`);
   }
   const { name, target } = imageNameForProposalTest(proposal);
+  const opts = buildOptsFor(name);
   // 'load' to ensure the images are output to the Docker client. Seems to be necessary
   // for the CI docker/build-push-action to re-use the cached stages.
-  const cmd = `docker buildx build ${buildOpts} --load --tag ${name} --target ${target} .`;
+  const cmd = `docker buildx build ${opts} --load --tag ${name} --target ${target} .`;
   console.log(cmd);
   if (!dry) {
     execSync(cmd, { stdio: 'inherit' });
