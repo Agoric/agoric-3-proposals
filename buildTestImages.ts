b7d41b0765aec1c8c6dfb435a46f020d5af5fcae
Turadg Aleahmad <turadg@agoric.com>
2023-11-09 14:26:53 -0800

    ci: switch to inline cache

--- buildTestImages.ts
+++ buildTestImages.ts
@@ -21,24 +21,14 @@ const proposals = match
   ? allProposals.filter(p => p.proposalName.includes(match))
   : allProposals;
 
-function buildOptsFor(name: string) {
-  // use GitHub Actions Cache if enabled https://docs.docker.com/build/cache/backends/gha/
-  if (process.env.ACTIONS_RUNTIME_TOKEN) {
-    const refName = process.env.GITHUB_REF_NAME;
-    return `--cache-from type=gha,scope=${refName}-${name} --cache-to type=gha,mode=max,scope=${refName}-${name}`;
-  }
-  return '';
-}
-
 for (const proposal of proposals) {
   if (!dry) {
     console.log(`\nBuilding test image for proposal ${proposal.proposalName}`);
   }
   const { name, target } = imageNameForProposal(proposal, 'test');
-  const opts = buildOptsFor(name);
   // 'load' to ensure the images are output to the Docker client. Seems to be necessary
   // for the CI docker/build-push-action to re-use the cached stages.
-  const cmd = `docker buildx build ${opts} --load --tag ${name} --target ${target} .`;
+  const cmd = `docker buildx build --load --tag ${name} --target ${target} .`;
   console.log(cmd);
   if (!dry) {
     execSync(cmd, { stdio: 'inherit' });
