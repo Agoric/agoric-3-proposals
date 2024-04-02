a7f615744cf2ef3b268e239c1919408a5d07554f
Luqi Pan <luqi@agoric.com>
2024-04-02 11:23:19 -0700

    test: update keys.test.js to test environment variables are set correctly

--- proposals/16:upgrade-8/keys.test.js
+++ proposals/16:upgrade-8/keys.test.js
@@ -10,21 +10,21 @@ import {
 } from '@agoric/synthetic-chain/src/lib/constants.js';
 
 test('gov1 address', async t => {
-  t.is(GOV1ADDR, 'agoric1ee9hr0jyrxhy999y755mp862ljgycmwyp4pl7q');
+  t.is(process.env.GOV1ADDR, GOV1ADDR);
 });
 
 test('gov2 address', async t => {
-  t.is(GOV2ADDR, 'agoric1wrfh296eu2z34p6pah7q04jjuyj3mxu9v98277');
+  t.is(process.env.GOV2ADDR, GOV2ADDR);
 });
 
 test('gov3 address', async t => {
-  t.is(GOV3ADDR, 'agoric1ydzxwh6f893jvpaslmaz6l8j2ulup9a7x8qvvq');
+  t.is(process.env.GOV3ADDR, GOV3ADDR);
 });
 
 test('user1 address', async t => {
-  t.is(USER1ADDR, 'agoric1rwwley550k9mmk6uq6mm6z4udrg8kyuyvfszjk');
+  t.is(process.env.USER1ADDR, USER1ADDR);
 });
 
 test('validator address', async t => {
-  t.is(VALIDATORADDR, 'agoric1estsewt6jqsx77pwcxkn5ah0jqgu8rhgflwfdl');
+  t.is(process.env.VALIDATORADDR, VALIDATORADDR);
 });
