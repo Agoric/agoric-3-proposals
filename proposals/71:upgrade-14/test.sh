16dae05eb8cfc5aafa3fc4c2469d7c2c14fa17cc
usmanmani1122 <usman.sbf@gmail.com>
2024-12-23 13:24:41 +0000

    hack

--- proposals/71:upgrade-14/test.sh
+++ proposals/71:upgrade-14/test.sh
@@ -3,6 +3,6 @@
 # Place here any test that should be executed using the executed proposal.
 # The effects of this step are not persisted in further proposal layers.
 
-yarn ava
+# yarn ava
 
 # ./create-kread-item-test.sh
