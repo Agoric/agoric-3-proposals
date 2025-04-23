7733957f62b0b5c65ed3b8d5dc73b4f2156b198e
mujahidkay <mujahidkay@gmail.com>
2025-04-23 14:57:10 +0500

    feat: add proposal 92

---
+++ proposals/92:reset-psm-mintlimit/README.md
@@ -0,0 +1,3 @@
+# CoreEval proposal to reset MintLimit for USDC, USDT PSMs
+
+Files under `submission` are taken from the relevant [release](https://github.com/Agoric/agoric-sdk/releases/tag/psm-param-fix).
---
+++ proposals/92:reset-psm-mintlimit/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+
+source /usr/src/upgrade-test-scripts/env_setup.sh
+
+pair="IST.USDC"
+NOBLE_USDC_DENOM="ibc/FE98AAD68F02F03565E9FA39A5E627946699B2B07115889ED812D8BA639576A9"
+
+echo DEBUG test mint limit was adjusted
+agd query vstorage data published.psm.${pair}.governance
+
+# there are two instances in governance vstorage path, use latest
+test_val $(agd query vstorage data published.psm.${pair}.governance | sed "s/^value: '//" | sed "s/'$//" | tr -d '\n' | jq -r '.values[-1] | fromjson | .body | sub("^#";"") | fromjson | .current.MintLimit.value.value | ltrimstr("+")') "500000000000" "PSM MintLimit was reset to 500K"
+
+agd tx --keyring-backend=test bank send validator $GOV1ADDR "2000000000${NOBLE_USDC_DENOM}" --from=provision --chain-id=agoriclocal --yes
+
+waitForBlock 3
+agd q bank balances $GOV1ADDR --output=json
+echo DEBUG execute PSM swap
+SWAP_OFFER=$(mktemp -t agops.XXX)
+agops psm swap --pair ${pair} --wantMinted 1001.00 --feePct 0.10 >|"$SWAP_OFFER"
+agops perf satisfaction --from $GOV1ADDR --executeOffer "$SWAP_OFFER" --keyring-backend=test
