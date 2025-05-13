d8af80d7565410372a54243f8fd1fe27d882dfa5
Richard Gibson <richard.gibson@gmail.com>
2025-05-13 12:54:31 -0400

    chore(synthetic-chain): Better document proposal eval configuration
    **eval.sh** vs. core-eval scripts/permits/bundles in **submission/**.

--- packages/synthetic-chain/public/upgrade-test-scripts/run_eval.sh
+++ packages/synthetic-chain/public/upgrade-test-scripts/run_eval.sh
@@ -13,22 +13,24 @@ fi
 
 startAgd
 
-echo "[$PROPOSAL] Agd started. Running CoreEval submission."
+echo "[$PROPOSAL] Agd started. Submitting proposal."
 cd /usr/src/proposals/"$PROPOSAL/" || fail "Proposal $PROPOSAL does not exist"
 
 if [ -f "eval.sh" ]; then
-    # this is what the script used to do. Also allows a proposal to override how they are eval-ed
+    # Allows a proposal to control its evaluation; used in particular for
+    # param-change proposals but also valid for other types such as core-eval.
     echo "[$PROPOSAL] Running eval.sh"
     ./eval.sh
 else
-    # newer proposals declare a submission
-    echo "[$PROPOSAL] Running proposal declared in package.json"
+    # Straightforward core-eval proposals can be specified directly in a "submission" directory
+    # containing $name.js scripts, $name-permit.json permits, and b1-$hash.json bundles.
+    echo "[$PROPOSAL] Running core-evals from submission/"
     # copy to run in the proposal package so the dependencies can be resolved
     cp /usr/src/upgrade-test-scripts/eval_submission.js .
     ./eval_submission.js
 fi
 
-echo "[$PROPOSAL] Eval completed. Running 10 blocks and exiting."
+echo "[$PROPOSAL] Proposal eval completed. Running 10 blocks and exiting."
 waitForBlock 10
 
 killAgd
