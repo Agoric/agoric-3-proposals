e264d874207fce9827d3c1f4cac07536052508ed
Dan Connolly <dckc@madmode.com>
2025-09-15 10:46:04 -0500

    chore: start 109 by copying 61

---
+++ proposals/109:reduce-max-validators/eval.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Exit when any command fails
+set -e
+
+source /usr/src/upgrade-test-scripts/env_setup.sh
+
+# shellcheck disable=SC2086
+agd tx gov submit-proposal param-change lower-bundle-cost.json \
+	${SIGN_BROADCAST_OPTS="--missing-env-setup"}
+
+voteLatestProposalAndWait
