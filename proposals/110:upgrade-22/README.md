546db045344db7110439196672d4bd7599bac8fb
mujahidkay <mujahidkay@gmail.com>
2025-11-06 20:07:48 +0500

    feat: add agoric-upgrade-22 (proposal 110)
    uses u22b tags, image and commit hashes

---
+++ proposals/110:upgrade-22/README.md
@@ -0,0 +1,5 @@
+# Proposal to upgrade the chain software
+
+This reflects agoric-upgrade-22, which was adopted on mainnet by [governance
+proposal 110](https://ping.pub/agoric/gov/110) at block height 21690000 and
+block time 2025-09-22T14:58:21.6012506Z.
---
+++ proposals/110:upgrade-22/test/initial.test.js
@@ -0,0 +1,24 @@
+import test from 'ava';
+import '@endo/init/debug.js';
+
+import { getVatDetails } from '@agoric/synthetic-chain';
+
+const expectedVatDetails = {
+  bank: { incarnation: 2 },
+  network: { incarnation: 3 },
+  ibc: { incarnation: 3 },
+  localchain: { incarnation: 3 },
+  orchestration: { incarnation: 2 },
+  provisionPool: { incarnation: 2 },
+  transfer: { incarnation: 3 },
+  walletFactory: { incarnation: 9 },
+  zoe: { incarnation: 3 },
+};
+
+test('post-upgrade vat details', async t => {
+  const actualVatDetails = {};
+  for await (const vatName of Object.keys(expectedVatDetails)) {
+    actualVatDetails[vatName] = await getVatDetails(vatName);
+  }
+  t.like(actualVatDetails, expectedVatDetails);
+});
